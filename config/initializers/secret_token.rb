a6a5bdfad6436538ded09711d7c0c0e9928bde61
kishan <kishan@kishan-H61MLV.(none)>
2014-02-19 18:41:25 +0530

    new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seemandhracinemas::Application.config.secret_token = 'a1dfba11e8ad8d6e599b79deef9ea4c75fb35dfe0279e54704f50e249bd61128f71794d1d7441f9a6914614bd2a65e142e7e35b252fd0f6c3efa20211819e027'
